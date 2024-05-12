470cd73cef56bd5352c7ff92b3c3db2337396724
mirajulhq <mirajul.hoque@cliqpack.com>
2024-05-12 07:39:39 +0600

    descending number problem solved

--- deteriorating_number.cpp
+++ deteriorating_number.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int max_number = 1000, min_number = 1, countExecution = 0;
+    while (max_number != 0)
+    {
+        countExecution++;
+        cout << max_number << '\t';
+        if (countExecution == 5)
+        {
+            cout << '\n';
+            countExecution = 0;
+        }
+        max_number--;
+    }
+    return 0;
+}
 
